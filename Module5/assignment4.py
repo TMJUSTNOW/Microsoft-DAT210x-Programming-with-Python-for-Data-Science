d5ec282313b82b54215c066b22fe4b9a3e7da1ac
khemon <khemonbeh@gmail.com>
2016-12-11 15:46:42 +0100

    assignement 4

--- Module5/assignment4.py
+++ Module5/assignment4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-
+from sklearn.cluster import KMeans;
 import matplotlib.pyplot as plt
 import matplotlib
 
@@ -35,7 +35,7 @@ def drawVectors(transformed_features, components_, columns, plt):
   import math
   important_features = { columns[i] : math.sqrt(xvector[i]**2 + yvector[i]**2) for i in range(num_columns) }
   important_features = sorted(zip(important_features.values(), important_features.keys()), reverse=True)
-  print "Projected Features by importance:\n", important_features
+  print("Projected Features by importance:\n", important_features)
 
   ax = plt.axes()
 
@@ -60,7 +60,9 @@ def doKMeans(data, clusters=0):
   # and fit it against your data. Then, return a tuple containing the cluster
   # centers and the labels
   #
-  # .. your code here ..
+  model = KMeans(n_clusters=clusters)
+  model.fit(data)
+  labels = model.predict(data)
   return model.cluster_centers_, model.labels_
 
 
@@ -70,7 +72,10 @@ def doKMeans(data, clusters=0):
 # for this dataset, since if the value is missing, you can assume no $ was spent
 # on it.
 #
-# .. your code here ..
+df = pd.read_csv('./Datasets/Wholesale customers data.csv')
+df.fillna(0)
+
+
 
 #
 # TODO: As instructed, get rid of the 'Channel' and 'Region' columns, since
@@ -78,16 +83,15 @@ def doKMeans(data, clusters=0):
 # than a national / international one. Leaving these fields in here would cause
 # KMeans to examine and give weight to them.
 #
-# .. your code here ..
-
+df = df.drop(['Channel','Region'], axis = 1)
 
 #
 # TODO: Before unitizing / standardizing / normalizing your data in preparation for
 # K-Means, it's a good idea to get a quick peek at it. You can do this using the
 # .describe() method, or even by using the built-in pandas df.plot.hist()
 #
-# .. your code here ..
-
+print(df.describe())
+print(df.plot.hist())
 
 #
 # INFO: Having checked out your data, you may have noticed there's a pretty big gap
@@ -118,9 +122,9 @@ for col in df.columns:
 # to, if there is a single row that satisfies the drop for multiple columns.
 # Since there are 6 rows, if we end up dropping < 5*6*2 = 60 rows, that means
 # there indeed were collisions.
-print "Dropping {0} Outliers...".format(len(drop))
+print("Dropping {0} Outliers...".format(len(drop)))
 df.drop(inplace=True, labels=drop.keys(), axis=0)
-print df.describe()
+print(df.describe())
 
 
 #
@@ -175,8 +179,8 @@ print df.describe()
 #T = preprocessing.StandardScaler().fit_transform(df)
 #T = preprocessing.MinMaxScaler().fit_transform(df)
 #T = preprocessing.MaxAbsScaler().fit_transform(df)
-#T = preprocessing.Normalizer().fit_transform(df)
-T = df # No Change
+T = preprocessing.Normalizer().fit_transform(df)
+#T = df # No Change
 
 
 #
@@ -197,8 +201,11 @@ centroids, labels = doKMeans(T, n_clusters)
 #
 # TODO: Print out your centroids. They're currently in feature-space, which
 # is good. Print them out before you transform them into PCA space for viewing
-#
-# .. your code here ..
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(centroids[:, 0], centroids[:, 1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
+plt.show()
+
 
 
 # Do PCA *after* to visualize the results. Project the centroids as well as 
@@ -233,6 +240,6 @@ if PLOT_VECTORS: drawVectors(T, display_pca.components_, df.columns, plt)
 
 # Add the cluster label back into the dataframe and display it:
 df['label'] = pd.Series(labels, index=df.index)
-print df
+print(df)
 
 plt.show()
